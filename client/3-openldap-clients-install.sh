8ed5692822da88802d4ccb2b08c156367b1df06e
Bernhard <bernhard.walter@me.com>
2018-03-11 12:19:40 +0100

    install ldap-utils instead of alias openldap-utils

--- client/3-openldap-clients-install.sh
+++ client/3-openldap-clients-install.sh
@@ -3,13 +3,13 @@ DIR=$(dirname $0) && source "$DIR/../config.sh" && source "$DIR/../lib.sh"
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-loginfo "3 Installing OpenLDAP cliewnt module"
+loginfo "3 Installing OpenLDAP client module"
 
 if is_centos7; then
     yum install -y openldap-clients
 elif is_ubuntu16; then
     export DEBIAN_FRONTEND=noninteractive
-    apt-get install -y openldap-utils
+    apt-get install -y ldap-utils
 elif is_sles12; then
     zypper install -y openldap2-client
 else
