c1ff6a685fda42f28f75952400e665db89bb74de
Bernhard Walter <b_walter@arcor.de>
2018-03-11 18:22:03 +0100

    fixed docker build results

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source ./config.sh
+source ./config.sh && source ./lib.sh
 
 VERSION=$1
 if [ "x$1" == "x" ]; then
@@ -9,32 +9,46 @@ fi
 
 if [ ${USE_KRB5} -eq 1 ]; then
     NAME="ldap-kdc"
+    KRB5_PORTS="-p 464:464 -p 749:749"
 else
     NAME="ldap"
+    KRB5_PORTS=""
 fi
 IMAGE="${NAME}-${DOMAIN}"
-
-tar -zcf installer.tar.gz bin/ server/ lib.sh config.sh create-server.sh
-docker build -t ${IMAGE}:${VERSION} .
-rm installer.tar.gz
 
 rm -fr ./target/*
 mkdir -p ./target
 
-docker run -d --name ldap ${IMAGE}:${VERSION} 
-docker cp ldap:/etc/ssl/certs/cacert.pem ./target
-docker rm -f ldap
+loginfo "Building docker image ..."
+tar -zcf installer.tar.gz bin/ server/ lib.sh config.sh create-server.sh
+docker build -t ${IMAGE}:${VERSION} .
+rm installer.tar.gz
 
-cp config.sh lib.sh ./target/
-tar -czf ./target/client-installer.tgz create-client.sh client/
-
+loginfo "Saving docker image ${IMAGE}:${VERSION} ..."
 docker save -o ./target/${IMAGE}-${VERSION}.docker ${IMAGE}:${VERSION} 
 
-cat << EOF > ./target/run.sh
-#!/bin/bash
-docker run -d -p 88:88 -p 389:389 -p 464:464 -p 636:636 -p 749:749 -p 8389:8389 --name ${NAME} ${IMAGE}:${VERSION}
-EOF
-chmod a+x ./target/run.sh
+loginfo "Extracting CA certificate from docker images ..."
+docker rm -f ldap-build 2> /dev/null
+docker run -d --name ldap-build ${IMAGE}:${VERSION} 
+docker cp ldap-build:/etc/ssl/certs/cacert.pem .
+docker rm -f ldap-build
 
-echo "Results in ./target:"
-ls -lh ./target
+loginfo "Creating client installer archive client-installer.tgz ..."
+tar -czf ./target/client-installer.tgz cacert.pem create-client.sh config.sh lib.sh client/
+
+cd target
+
+loginfo "Creating server archive server.tar.bz2 ..."
+cat << EOF > ./run.sh
+#!/bin/bash
+docker load -i ${IMAGE}-${VERSION}.docker
+docker run -d -p 88:88 -p 389:389 -p 636:636 ${KRB5_PORTS} -p 8389:8389 --name ${NAME} ${IMAGE}:${VERSION}
+EOF
+chmod a+x ./run.sh
+
+tar -jcf server.tar.bz2 ${IMAGE}-${VERSION}.docker run.sh
+
+rm ${IMAGE}-${VERSION}.docker run.sh
+
+loginfo "Results in ./target:"
+ls -lh
