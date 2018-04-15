b30863323f4bbbbcdda859cc102ab48ce148a535
Bernhard Walter <b_walter@arcor.de>
2018-04-15 20:08:28 +0200

    default server for standalone is docker

--- config-standalone.sh
+++ config-standalone.sh
@@ -1,3 +1,3 @@
 export DOMAIN=acme.localdomain
-export SERVER_NAME=authx        # name of the ldap/krb5 authentication server
+export SERVER_NAME=docker       # name of the ldap/krb5 authentication server
 export SERVER_IP=192.168.56.20  # ip of the ldap/krb5 authentication server
